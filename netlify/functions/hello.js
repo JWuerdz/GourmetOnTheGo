32bdaae5be4ec28811bcc0aa269b107b17e5c458
Jwuerdz <21118036@saultcollege.ca>
2025-03-07 18:06:40 -0500

    trying to call the database

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require("mongodb");
 
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const mongoClient = new MongoClient(process.env.MONGODB_URI || "mongodb+srv://admin:admin@gourmet2go.5jpsj.mongodb.net/");
 
 const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
-        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE || "gourmet2go");
         const collection = database.collection("items");
         const results = await collection.find({}).limit(10).toArray();
         return {
