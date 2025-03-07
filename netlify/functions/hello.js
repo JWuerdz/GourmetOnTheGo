f50ce54d8a8274b068d2395f934a0bad0d171358
Jwuerdz <21118036@saultcollege.ca>
2025-03-07 18:16:19 -0500

    readded the .env

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require("mongodb");
 
-const mongoClient = new MongoClient(process.env.MONGODB_URI || "mongodb+srv://admin:admin@gourmet2go.5jpsj.mongodb.net/");
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
 const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
     try {
-        const database = (await clientPromise).db(process.env.MONGODB_DATABASE || "gourmet2go");
-        const collection = database.collection("items");
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
         const results = await collection.find({}).limit(10).toArray();
         return {
             statusCode: 200,
