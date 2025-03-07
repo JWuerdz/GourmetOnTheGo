b9904d5b23c05ec14196c85fe7a29cb6c1e715e3
Jwuerdz <21118036@saultcollege.ca>
2025-03-07 17:59:39 -0500

    database test

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,21 @@
-exports.handler = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
-    };
-  };
-  
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection("items");
+        const results = await collection.find({}).limit(10).toArray();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    }
+}
+
+module.exports = { handler }
