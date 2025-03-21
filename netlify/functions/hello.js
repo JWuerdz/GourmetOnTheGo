5ee7bfc080d49c3fb7379b2b881f7b234a048abd
Brock McClelland <23034226@saultcollege.ca>
2025-03-21 19:19:58 -0400

    fix

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const DB_SOURCE = "../../data/gourmet2go.sqlite";
+const DB_SOURCE = "/netlify/functions/data/gourmet2go.sqlite";
 const TEMP_DB = "/tmp/gourmet2go.sqlite";
 
 // Ensure the source database exists before copying
