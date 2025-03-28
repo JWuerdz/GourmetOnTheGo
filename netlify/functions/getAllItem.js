3226dc69f825436a1bcbdbeb21e84e102f31f86c
Brock McClelland <23034226@saultcollege.ca>
2025-03-28 17:52:56 -0400

    added getAllItems

---
+++ netlify/functions/getAllItem.js
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = process.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function handler(event, context) {
+  try {
+    const { data, error } = await supabase
+      .from('Items')
+      .select('*')
+      ;
+
+    
+    if (error) throw error;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
