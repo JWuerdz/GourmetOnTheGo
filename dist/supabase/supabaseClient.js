199f32b6403b3c7e648638f5784ea6722e1acbf5
Jwuerdz <21118036@saultcollege.ca>
2025-04-15 17:12:36 -0400

    updated changes

--- dist/supabase/supabaseClient.js
+++ dist/supabase/supabaseClient.js
@@ -1,9 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL; 
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY; 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
